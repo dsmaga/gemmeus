c35fdbde5b4dc12ca6ad6cc12c666e4d51824c75
Chris Dosé <chris.dose@gmail.com>
2014-01-25 10:56:37 -0800

    Added tile swapping logic.

--- game.js
+++ game.js
@@ -1,18 +1,16 @@
 function Game(n, types) {
 
+  var _game = this
+
   function Tile(type) {
-    var _self = this
+    var _tile = this
 
     this.type = type
     this.selected = ko.observable(false)
 
     this.tileClass = ko.computed(function() {
-      return 'tileType' + type + (_self.selected() ? ' selected' : '')
+      return 'tileType' + type + (_tile.selected() ? ' selected' : '')
     })
-
-    this.select = function select() {
-      _self.selected(true)
-    }
   }
 
   this.columns = _.range(n).map(function() {
@@ -27,11 +25,38 @@ function Game(n, types) {
     })
   }
 
+  this.select = function select(tile) {
+    if(_game.source) {
+      _game.swapTiles(_game.source, tile)
+      _game.source.selected(false)
+      _game.source = null
+    }
+    else {
+      _game.source = tile
+      tile.selected(true)
+    }
+  }
+
+  this.swapTiles = function swapTiles(tile1, tile2) {
+    function findTileCoords(tile) {
+      var i, j
+      for(i = 0; i < n; i++) {
+        j = _game.columns[i].indexOf(tile)
+        if(j !== -1) break
+      }
+    return [i, j]
+    }
+    var tile1Coords = findTileCoords(tile1)
+      , tile2Coords = findTileCoords(tile2)
+    _game.columns[tile1Coords[0]].splice(tile1Coords[1], 1, tile2)
+    _game.columns[tile2Coords[0]].splice(tile2Coords[1], 1, tile1)
+  }
+
   this.fillGrid()
 
 }
 
-var game = new Game(5, 4)
+var game = new Game(10, 5)
 
 $(function() {
   ko.applyBindings(game)
