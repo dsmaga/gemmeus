7d5e1df7eb78b1a0329530b61bc6dc538177d639
Chris Dosé <chris.dose@gmail.com>
2014-01-25 10:23:55 -0800

    Basic game grid rendering and tile selecting.

--- game.js
+++ game.js
@@ -1,7 +1,38 @@
-function Game() {
-  this.foo = 'bar'
+function Game(n, types) {
+
+  function Tile(type) {
+    var _self = this
+
+    this.type = type
+    this.selected = ko.observable(false)
+
+    this.tileClass = ko.computed(function() {
+      return 'tileType' + type + (_self.selected() ? ' selected' : '')
+    })
+
+    this.select = function select() {
+      _self.selected(true)
+    }
+  }
+
+  this.columns = _.range(n).map(function() {
+    return ko.observableArray()
+  })
+
+  this.fillGrid = function fillGrid() {
+    this.columns.forEach(function(col) {
+      col(_.range(n).map(function() {
+        return new Tile(_.random(1, types))
+      }))
+    })
+  }
+
+  this.fillGrid()
+
 }
 
-var game = new Game()
+var game = new Game(5, 4)
 
-console.log(game.foo)
+$(function() {
+  ko.applyBindings(game)
+})
