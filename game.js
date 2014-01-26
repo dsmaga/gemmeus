7dd4f32df764dedde32ded9b021f44c1b88d0546
Chris Dosé <chris.dose@gmail.com>
2014-01-26 17:30:52 -0600

    Major refactor after discovering the actual rules to the game... :)

--- game.js
+++ game.js
@@ -9,6 +9,104 @@ function Game(n, types) {
     this.offsetClass = ko.computed(function() {
       return 'offset' + (n - _column.tiles().length)
     })
+
+    this.tilesReversed = ko.computed(function() {
+      return Array.prototype.slice.call(_column.tiles()).reverse()
+    })
+  }
+
+  function TileGrid(n) {
+    var _grid = this
+
+    this.columns = _.range(n).map(function(){return new Column})
+
+    this.getTile = function(i, j) {
+      if(i < 0 || i >= n) return void(0)
+      return _grid.columns[i].tiles()[j]
+    }
+
+    this.horizontalRunCount = function(i, j, type) {
+      var x
+        , count = 1
+        , tile
+
+      if(!type) type = getTile(i, j).type
+
+      for(x = i + 1; x < n; x++) {
+        tile = _grid.getTile(x, j)
+        if(!tile) break
+        else if(tile.type !== type) break
+        count++
+      }
+      for(x = i - 1; x > 0; x--) {
+        tile = _grid.getTile(x, j)
+        if(!tile) break
+        else if(tile.type !== type) break
+        count++
+      }
+      return count
+    }
+
+    this.verticalRunCount = function(i, j, type) {
+      var y
+        , count = 1
+        , tile
+
+      if(!type) type = getTile(i, j).type
+
+      for(y = j + 1; y < n; y++) {
+        tile = _grid.getTile(i, y)
+        if(!tile) break
+        else if(tile.type !== type) break
+        count++
+      }
+      for(y = j - 1; y > 0; y--) {
+        tile = _grid.getTile(i, y)
+        if(!tile) break
+        else if(tile.type !== type) break
+        count++
+      }
+      return count
+    }
+
+    this.addTile = function(x) {
+      var avoidTypes = []
+        , typesAvailable
+        , column = _grid.columns[x]
+        , testTypes
+        , y
+
+      y = column.tiles().length
+
+      testTypes = _.filter( [ _grid.getTile(x-1, y)
+                            , _grid.getTile(x, y-1)
+                            , _grid.getTile(x+1, y)
+                            ]
+                          ).map(function(tile) { return tile.type })
+
+      testTypes.forEach(function(type) {
+        if(_grid.verticalRunCount(x, y, type) >= 2) avoidTypes.push(type)
+        if(_grid.horizontalRunCount(x, y, type) >= 2) avoidTypes.push(type)
+      })
+
+      typesAvailable = _.difference(_.range(1, types+1), avoidTypes)
+      column.tiles.push(new Tile(_.sample(typesAvailable)))
+    }
+
+    ko.computed(function() {
+      _grid.columns.forEach(function(column, i) {
+        if(column.tiles().length < n) {
+          _grid.addTile(i)
+        }
+      })
+    }).extend({throttle: 250})
+
+    // this gets the computed started, dunno why it's needed...
+    window.setTimeout(function() {
+      _.range(n).forEach(function(i) {
+        _grid.addTile(i)
+      })
+    }, 250)
   }
 
   function Tile(type) {
@@ -22,19 +120,7 @@ function Game(n, types) {
     })
   }
 
-  this.columns = _.range(n).map(function() {
-    return new Column
-  })
-
-  this.fillGrid = function fillGrid() {
-    this.columns.forEach(function(column) {
-      column.tiles(_.range(n).map(function() {
-        return new Tile(_.random(1, types))
-      }))
-    })
-  }
-
-  this.select = function select(tile) {
+  this.select = function(tile) {
     if(_game.source) {
       _game.swapTiles(_game.source, tile)
       _game.source.selected(false)
@@ -46,26 +132,26 @@ function Game(n, types) {
     }
   }
 
-  this.swapTiles = function swapTiles(tile1, tile2) {
+  this.swapTiles = function(tile1, tile2) {
     function findTileCoords(tile) {
       var i, j
       for(i = 0; i < n; i++) {
-        j = _game.columns[i].tiles.indexOf(tile)
+        j = _game.grid.columns[i].tiles.indexOf(tile)
         if(j !== -1) break
       }
     return [i, j]
     }
     var tile1Coords = findTileCoords(tile1)
       , tile2Coords = findTileCoords(tile2)
-    _game.columns[tile1Coords[0]].tiles.splice(tile1Coords[1], 1, tile2)
-    _game.columns[tile2Coords[0]].tiles.splice(tile2Coords[1], 1, tile1)
+    _game.grid.columns[tile1Coords[0]].tiles.splice(tile1Coords[1], 1, tile2)
+    _game.grid.columns[tile2Coords[0]].tiles.splice(tile2Coords[1], 1, tile1)
   }
 
-  this.fillGrid()
+  this.grid = new TileGrid(n)
 
 }
 
-var game = new Game(10, 5)
+var game = new Game(10, 7)
 
 $(function() {
   ko.applyBindings(game)
